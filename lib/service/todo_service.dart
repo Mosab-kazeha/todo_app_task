7cd2f7dbbebc5f631453befdfc3a546934b04b30
Mosab-kazeha <mk.lk.mosab@gmail.com>
2023-12-18 14:54:55 +0300

    error with get data

--- lib/service/todo_service.dart
+++ lib/service/todo_service.dart
@@ -1,9 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:todo_app_task/model/todo_model.dart';
 import 'package:todo_app_task/service/base_service.dart';
 
 abstract class TodoService extends BaseService {
-  Future<List<TodoModel>> getAllTodo();
-  Future<TodoModel> getOneTodo();
+  /*Future<List<TodoModel>>*/ getAllTodo();
+  Future<TodoModel> getOneTodo({required String id});
   createNewTodo({required TodoModel newTodo});
   updateOldTodo({required String id, required TodoModel newTodo});
   deleteTodo(String id);
@@ -23,15 +24,22 @@ class Todo extends TodoService {
   }
 
   @override
-  Future<List<TodoModel>> getAllTodo() {
-    // TODO: implement getAllTodo
-    throw UnimplementedError();
+  /* Future<List<TodoModel>>*/ getAllTodo() async {
+    print(" enter to getAllTodo ");
+    Response response =
+        await dio.get("https://657ec9263e3f5b18946421b8.mockapi.io/todo");
+    print(" get the response $response");
+    return response;
   }
 
   @override
-  Future<TodoModel> getOneTodo() {
-    // TODO: implement getOneTodo
-    throw UnimplementedError();
+  Future<TodoModel> getOneTodo({required String id}) async {
+    Response response = await dio.get("$baseUrl/$id");
+    dynamic temp = response.data;
+    TodoModel result = TodoModel.fromMap(temp.data[id]);
+
+    print("hi $result");
+    return result;
   }
 
   @override
